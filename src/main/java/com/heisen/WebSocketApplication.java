246e22225a4a2cc9c6f991f2cc1d5356cb2dc25f
wenmingyue <664461395@qq.com>
2024-06-06 10:21:20 +0800

    初始化

---
+++ src/main/java/com/heisen/WebSocketApplication.java
@@ -0,0 +1,16 @@
+package com.heisen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author sen
+ * @description
+ * @date 2024/5/31 9:22
+ */
+@SpringBootApplication
+public class WebSocketApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebSocketApplication.class, args);
+    }
+}
